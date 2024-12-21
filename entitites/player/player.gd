2d86ba50465fef3f96ac03caf2cc7317dabe07ab
akrck02 <aketza.vazquez@gmail.com>
2024-12-21 21:36:36 +0100

    pathfinding: now configurable for isometric and regular 2D

--- entitites/player/player.gd
+++ entitites/player/player.gd
@@ -4,29 +4,32 @@ extends CharacterBody2D
 @export var tilemap : NavigableTilemapLayer = null;
 @onready var navigation : Navigation = $Navigation
 @onready var timer : Timer = $Timer
-var tween : Tween 
+var tween : Tween
 var moving : bool = false
 var steps : PackedVector2Array = []
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and Input.is_action_just_pressed(&'ui_accept'):
-			
+
 		var debug = get_parent().get_tree().get_nodes_in_group("debug")[0]
 		for child in debug.get_children():
-			child.queue_free() 
-			
+			child.queue_free()
+
 		var new_position = Positions.convert_ui_position_to_scene_global_position(get_viewport(), event.position)
 		var current_coordinates = tilemap.get_coordinates_from_global_position(global_position)
 		var new_coordinates = tilemap.get_coordinates_from_global_position(new_position)
-		
+
 		steps = navigation.get_steps(tilemap, current_coordinates, new_coordinates)
+		print("Steps from %s to %s" % [current_coordinates, new_coordinates])
+		print("%s\n" %steps)
+
 		_draw_line(debug, steps)
 
 func _process(_delta: float) -> void:
 
 	if moving or steps.is_empty():
 		return
-		
+
 	var step = steps[0]
 	steps.remove_at(0)
 	_move_to_cell(step)
@@ -35,18 +38,19 @@ func _process(_delta: float) -> void:
 func _draw_line(parent : Node2D, draw_steps: PackedVector2Array) -> void:
 	var line : Line2D = Line2D.new()
 	for point in draw_steps:
-		line.add_point(Vector2(point.x + 8, point.y))
-	
-	line.default_color = Color.BLACK
-	line.default_color.a = .25
+		line.add_point(Vector2(point.x, point.y))
+
+	line.default_color = Color.REBECCA_PURPLE
+	line.default_color.a = 1
 	line.visible = true
 	line.width = 1
+	line.z_index = 999
 	line.joint_mode = Line2D.LINE_JOINT_BEVEL
 	parent.add_child(line)
-	
+
 
 func _move_to_cell(new_position: Vector2) -> void:
-	
+
 	moving = true
 	tween = create_tween()
 	tween.tween_property(self, "position", new_position, 1.00 / 3)
@@ -54,10 +58,10 @@ func _move_to_cell(new_position: Vector2) -> void:
 	moving = false
 
 func _input(_event: InputEvent) -> void:
-	
+
 	if Input.is_action_just_pressed(&"ui_reset"):
 		get_tree().reload_current_scene()
-	
+
 	if Input.is_action_just_pressed(&"ui_cancel"):
 		get_tree().change_scene_to_file('res://main.tscn')
 
--- entitites/tilemap/navigable_tilemap_layer.gd
+++ entitites/tilemap/navigable_tilemap_layer.gd
@@ -1,60 +1,91 @@
 class_name NavigableTilemapLayer
 extends TileMapLayer
 
-@export var debug_canvas_layer : TileMapLayer
+const MIN_INTEGER_VALUE : int = -9223372036854775808
+const MAX_INTEGER_VALUE : int = 9223372036854775807
+const DEFAULT_SPRITE_SIZE : int = 16
+
+const WALL_TYPE : int = 1
+const TYPE_PROPERTY_NAME : String = "type"
+
+## Enumeration for the tilemap modes
+enum TilemapMode {
+	SQUARE,
+	ISOMETRIC_DOWN,
+	ISOMETRIC_RIGHT
+}
+
+## Enumeration for diagonal movement policy
+enum DiagonalPolicy {
+	NEVER,
+	ALWAYS,
+	ONLY_IF_NO_OBSTACLES
+}
+
+@export_category("Configuration")
+@export var tilemap_mode : TilemapMode = TilemapMode.SQUARE
+@export var diagonal_policy : DiagonalPolicy = DiagonalPolicy.NEVER
+@export var cell_size : Vector2i = Vector2i(DEFAULT_SPRITE_SIZE, DEFAULT_SPRITE_SIZE)
 @export var layers : Array[TileMapLayer] = []
-@onready var map_limits : Rect2i = get_used_rectangle()
+
+@export_category("Debug")
+@export var debug_canvas_layer : TileMapLayer
+
 var grid : AStarGrid2D = AStarGrid2D.new()
 
 func _ready() -> void:
-	setup_grid()
 
-func setup_grid() -> void:
-	grid.cell_size = Vector2(16, 16)
-	grid.cell_shape = AStarGrid2D.CELL_SHAPE_SQUARE
-	grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
-	grid.region = map_limits
-	grid.update()
-	update_walls()
-	
-	for cell in get_used_cells():
-		debug_canvas_layer.set_cell(cell,0,Vector2(0,3),0)
+	match tilemap_mode:
+		TilemapMode.SQUARE: grid.cell_shape = AStarGrid2D.CELL_SHAPE_SQUARE
+		TilemapMode.ISOMETRIC_DOWN: grid.cell_shape = AStarGrid2D.CELL_SHAPE_ISOMETRIC_DOWN
+		TilemapMode.ISOMETRIC_RIGHT: grid.cell_shape = AStarGrid2D.CELL_SHAPE_ISOMETRIC_RIGHT
 
-func update_walls():
+	match diagonal_policy:
+		DiagonalPolicy.NEVER: grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+		DiagonalPolicy.ALWAYS: grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ALWAYS
+		DiagonalPolicy.ONLY_IF_NO_OBSTACLES: grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
+
 	layers.append(self)
+	grid.cell_size = cell_size
+	grid.region = get_used_rectangle()
+	grid.update()
+	set_current_walls()
+
+
+## Update the walls on the grid
+func set_current_walls():
+
 	for layer in layers:
 		for cell in layer.get_used_cells():
-			var data : TileData = layer.get_cell_tile_data(cell)
-			
-			if data.get_custom_data("type") == 1:
+			var data = layer.get_cell_tile_data(cell)
+			if data != null and data.get_custom_data(TYPE_PROPERTY_NAME) == WALL_TYPE:
+				print("Setting wall at: %s, %s" %[cell.x , cell.y])
 				grid.set_point_solid(cell, true)
+				debug_canvas_layer.set_cell(cell,2,Vector2(4,5),0)
 
 
+## Get the used rectangle of the tilemap
 func get_used_rectangle() -> Rect2i:
-	
-	var min_x : int = 9999999999999
-	var max_x : int = -9999999999999
-	
-	var min_y : int = 9999999999999
-	var max_y : int = -9999999999999
-	
+
+	var min_position = Vector2(MAX_INTEGER_VALUE, MAX_INTEGER_VALUE)
+	var max_position = Vector2(MIN_INTEGER_VALUE, MIN_INTEGER_VALUE)
+
 	for layer in layers:
-		for cell in layer.get_used_cells(): 
-			if min_x > cell.x:	min_x = cell.x
-			if max_x < cell.x:	max_x = cell.x	
-			if min_y > cell.y:	min_y = cell.y
-			if max_y < cell.y:	max_y = cell.y	
+		for cell in layer.get_used_cells():
+			if cell.x < min_position.x : min_position.x = cell.x
+			if cell.y < min_position.y : min_position.y = cell.y
+			if cell.x > max_position.x : max_position.x = cell.x
+			if cell.y > max_position.y : max_position.y = cell.x
 
-	var position : Vector2 = Vector2(min_x, min_y)
-	var size : Vector2 = Vector2(max_x - min_x, max_y - min_y)
-	return Rect2i(position, size)
+	var size : Vector2 = max_position - min_position
+	return Rect2i(min_position, Vector2.ONE + size )
 
 
-# calculate cell index
+## calculate cell index
 func calculate_point_index(point) -> Vector2i:
-	# subtract offset from position
-	point -= map_limits.position
+	point -= grid.region.position
 	return point
+
 
 ## Get coordinates from global position
 func get_coordinates_from_global_position(global_origin : Vector2) -> Vector2i:
